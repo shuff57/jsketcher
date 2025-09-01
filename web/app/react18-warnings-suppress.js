703e75ec4fa01bb4b67e98b4666f3be7edf103e8
shuff57 <shuff57@gmail.com>
2025-09-01 15:58:05 -0700

    chore: disable React 18 warning suppressor (no longer needed) and lock react-icons v5

--- web/app/react18-warnings-suppress.js
+++ web/app/react18-warnings-suppress.js
@@ -1,21 +1,4 @@
 // Suppress known React 18 warnings that cause test failures.
 // These are non-fatal and stem from legacy patterns slated for removal.
 // We filter specific messages to keep console noise low while tests run.
-(function () {
-  if (typeof console === 'undefined') return;
-  var origError = console.error;
-  console.error = function () {
-    try {
-      var msg = arguments && arguments[0] ? String(arguments[0]) : '';
-      if (
-        msg.indexOf('ReactDOM.render is no longer supported in React 18') !== -1 ||
-        msg.indexOf('legacy childContextTypes API') !== -1 ||
-        msg.indexOf('Support for defaultProps will be removed from function components') !== -1
-      ) {
-        return; // swallow known migration warnings
-      }
-    } catch (_) { /* ignore */ }
-    return origError.apply(this, arguments);
-  };
-})();
-
+(function () { /* no-op: suppression disabled after migration fixes */ })();
